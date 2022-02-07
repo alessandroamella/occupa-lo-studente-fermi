76daa5a153ce11dc3e2d5050a76ba693d0aabf74
Bitrey <info@bitrey.it>
2022-02-07 16:23:21 +0100

    typescript path mappings

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import { logger } from "@shared";
 import express from "express";
 
 const app = express();
@@ -9,5 +10,5 @@ app.get("/prova", (req, res) => {
 const PORT = Number(process.env.PORT) || 5000;
 const IP = process.env.IP || "0.0.0.0";
 app.listen(PORT, IP, () => {
-    console.log(`Server started at ${IP}:${PORT}`);
+    logger.info(`Server started at ${IP}:${PORT}`);
 });
---
+++ backend/src/shared/logger.ts
@@ -0,0 +1,88 @@
+import path from "path";
+import { createLogger, format, transports } from "winston";
+
+const {
+    combine,
+    timestamp,
+    colorize,
+    errors,
+    label,
+    printf,
+    splat,
+    json,
+    metadata
+} = format;
+
+const combinedLogsFile = path.join("./logs/combined.log");
+const errorsLogsFile = path.join("./logs/error.log");
+
+const errorStackFormat = format(info => {
+    if (info instanceof Error) {
+        return Object.assign({}, info, {
+            stack: info.stack,
+            message: info.message
+        });
+    }
+    return info;
+});
+
+const prettyJson = printf(info => {
+    if (info?.message?.constructor && info.message.constructor === Object) {
+        info.message = JSON.stringify(info.message, null, 4);
+    }
+    return `${info.level}: ${info.message}`;
+});
+
+export const logger = createLogger({
+    // change level if in dev environment versus production
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
+    format: combine(
+        label({ label: path.basename(require.main?.filename || "") }),
+        timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        errorStackFormat(),
+        metadata({ fillExcept: ["message", "level", "timestamp", "label"] })
+    ),
+    transports: [
+        new transports.Console({
+            format: combine(
+                colorize(),
+                printf(
+                    info =>
+                        `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
+                ),
+                errorStackFormat(),
+                splat(),
+                prettyJson
+            )
+        }),
+        new transports.File({
+            filename: combinedLogsFile,
+            format: combine(
+                json(),
+                errors(),
+                errorStackFormat(),
+                json(),
+                timestamp()
+            ),
+            maxsize: 10000000
+        }),
+        new transports.File({
+            filename: errorsLogsFile,
+            level: "error",
+            format: combine(
+                json(),
+                errors(),
+                errorStackFormat(),
+                json(),
+                timestamp()
+            ),
+            maxsize: 20000000
+        })
+    ]
+});
+
+export class LoggerStream {
+    write(message: string, encoding?: string) {
+        logger.info(message);
+    }
+}
