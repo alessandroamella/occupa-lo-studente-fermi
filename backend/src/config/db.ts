2094bc15ff0811ea69e3ab4e37cf669ca0b37033
Alessandro Amella <info@bitrey.it>
2022-03-07 10:41:46 +0100

    add missing envs

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,11 @@
 import { logger } from "@shared";
 import mongoose from "mongoose";
 
-if (!process.env.MONGOOSE_URI) {
-    logger.error("Missing MONGOOSE_URI env");
+if (!process.env.MONGODB_URI) {
+    logger.error("Missing MONGODB_URI env");
     process.exit(1);
 }
 
-mongoose.connect(process.env.MONGOOSE_URI, err => {
+mongoose.connect(process.env.MONGODB_URI, err => {
     err ? logger.error(err) : logger.info("Connected to MongoDB");
 });
